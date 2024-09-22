69783dcdb83be7d4b56499eca6031dd045cc4a11
Eli Evans <70294157+eli-evans149@users.noreply.github.com>
2024-09-22 04:15:47 +0000

    Get text directly out of file before randomizing

--- RandomIdeasDisplayer.java
+++ RandomIdeasDisplayer.java
@@ -2,6 +2,7 @@ public class RandomIdeasDisplayer
 {
     public static void main(String[] args)
     {
+        System.out.println("Now displaying the ideas from https://rosettacode.org/wiki/Category:Programming_Tasks in a random order.");
         RandomIdeasGenerator generator = new RandomIdeasGenerator();
         generator.displayRandomIdeas();
     }
--- RandomIdeasGenerator.java
+++ RandomIdeasGenerator.java
@@ -1,22 +1,44 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Collections;
+import java.net.URL;
+import java.io.File;
+import java.util.Scanner;
 
-public class RandomIdeasGenerator {
-    
+public class RandomIdeasGenerator
+{
     private ArrayList<String> ideas;
     
-    public RandomIdeasGenerator() {
+    public RandomIdeasGenerator()
+    {
         this.ideas = new ArrayList<String>();
-        //TODO: Make this better than just these two ideas.
-        this.ideas.add("idea 1");
-        this.ideas.add("idea 2");
-        Collections.shuffle(this.ideas);
+        
+        System.getProperty("user.dir");
+        File file = new File(System.getProperty("user.dir") + "//programming_tasks.txt");
+        try
+        {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine())
+            {
+                String currentLine = (scanner.nextLine());
+                
+                this.ideas.add(currentLine);
+            }
+
+            Collections.shuffle(this.ideas);
+        }
+        catch (Exception e) {
+            System.out.println("oops");
+            System.out.println("File name: " + file.getPath());
+            System.out.println(e.getMessage());
+        }
     }
     
-    public void displayRandomIdeas() {
+    public void displayRandomIdeas()
+    {
         Iterator<String> ideaIterator = ideas.iterator();
-        while (ideaIterator.hasNext()) {
+        while (ideaIterator.hasNext())
+        {
             System.out.println(ideaIterator.next());
         }
     }
